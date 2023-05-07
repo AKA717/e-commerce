426a1b77a9cd02d2faf995972c6b4e53c23383e6
amalthomas-exe <thomasamal479@gmail.com>
2023-05-07 20:34:15 +0530

    mongodb client added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+let MongoClient= require('mongodb').MongoClient;
+
+var url = 'mongodb://127.0.0.1:27017';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Monster' });
 });
+
+router.post('/submit',(req,res) => {
+  
+  console.log(req.body);
+
+  MongoClient.connect(url,(err,db) => {
+
+    if(!err)
+      console.log("database connected!");
+  })
+
+  });
 
 module.exports = router;
